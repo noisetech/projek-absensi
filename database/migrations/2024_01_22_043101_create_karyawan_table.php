60eeb3c489616487e6191c761f1e1f5c3ebf6ce7
noisetech <box92143@gmail.com>
2024-01-22 11:51:31 +0700

    set up awal

---
+++ database/migrations/2024_01_22_043101_create_karyawan_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKaryawanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('karyawan', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->string('jabatan');
+            $table->string('no_hp');
+            $table->string('email_pribadi')->nullable();
+            $table->longText('alamat_pribadi')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('karyawan');
+    }
+}
---
+++ database/migrations/2024_01_22_043324_create_presensi_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePresensiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('presensi', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('karyawan_id');
+            $table->date('tgl_presensi');
+            $table->time('jam_in');
+            $table->time('jam_out')->nullable();
+            $table->text('foto_in')->nullable();
+            $table->text('foto_out')->nullable();
+            $table->timestamps();
+
+            $table->foreign('karyawan_id')->references('id')
+                ->on('karyawan')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('presensi');
+    }
+}
